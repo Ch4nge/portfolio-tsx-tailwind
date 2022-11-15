32cc7d2dee86e5f955a136b221400665a77b3541
Sami Hautamäki <sami.hautamaki1@gmail.com>
2022-11-15 10:39:33 +0200

    Add logo

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -6,6 +6,7 @@ import Navigation from './Navigation';
 const Menu = () => {
       return (
         <div className="bg-gray-900 sticky flex flex-col top-0 bottom-0 left-0 h-screen w-32">
+	  <img class-name="pt-1 inline-block"src="./logo.png"/>
           <MenuSection styles="border-solid border-b border-gray-600"/>
           <Navigation/>
           <MenuSection/>
