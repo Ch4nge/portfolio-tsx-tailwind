c8a95dbe2414ba3dce5c086aa76eeb40c29d9f73
Sami Hautamäki <sami.hautamaki1@gmail.com>
2022-11-19 19:22:11 +0200

    Add SkillCard initial styles

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const SkillCard = () => {
       return (
-        <div className="bg-teal-300 w-96 h-64 inline-block flex">
-	  arstrst
+        <div className="bg-gray-900 border-solid border-t-2 rounded-b-md border-green-300 w-96 h-64 inline-block flex">
+	  Javascript
         </div>
       )
 }
